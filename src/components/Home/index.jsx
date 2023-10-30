57b6f7e070474653d5dfd4dd687ca61370a5a847
ShivaSirikonda <shivasirikonda6@gmail.com>
2023-10-30 19:44:23 +0530

    Sample Commit

---
+++ src/components/Home/index.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Navbar from "../Navbar";
+import { Link } from "react-router-dom";
+import "./index.css";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="banner-img">
+        <div className="title">
+          {/* <h3>
+            Find the <span>Right Job</span> In the
+            <br />
+            <span> Right Companies</span>
+          </h3> */}
+          <img src="https://upload.wikimedia.org/wikipedia/en/thumb/7/71/National_Institute_Of_Technology_Silchar_Logo.svg/800px-National_Institute_Of_Technology_Silchar_Logo.svg.png" height="300px" widht="100px"/>
+          <p>Got fired..!! Get Ready to be hired</p>
+          <div className="small-tagline">
+          </div>
+        </div>
+        <div className="button" data-testid="btn">
+          <Link to="/Jobs">Find Jobs</Link>
+        </div>
+      </div>
+      <div className="social-media" data-testid="images">
+        <img src="https://assets.website-files.com/5ec5d86528da2f24250d634c/5ec7175d7e0c401a3e668a1d_facebook-logo.svg" alt="fb" />
+        <img src="https://assets.website-files.com/5ec5d86528da2f24250d634c/5ec7175d68c9b0a57ed94925_google-logo.svg" alt="google" />
+        <img src="https://assets.website-files.com/5ec5d86528da2f24250d634c/601e13bc333df3521cce5b6a_youtube-logo-jobs-webflow-template.svg" alt="youtube" />
+        <img src="https://assets.website-files.com/5ec5d86528da2f24250d634c/601e13bc774d5a00bcbb0baf_linkedin-logo-jobs-webflow-template.svg" alt="linkedin" />
+      </div>
+    </>
+  );
+};
+
+export default Home;
